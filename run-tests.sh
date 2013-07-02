04e9c5f8b3c326efb681842bb31304947f68e196
Mark McLoughlin <markmc@redhat.com>
2013-07-02 07:02:16 +0100

    Make results easier to diff

--- run-tests.sh
+++ run-tests.sh
@@ -4,4 +4,6 @@ for t in $(ls -1 tests/* | xargs -i basename {}); do
     . ./setup.sh $t
     bash -x ./tests/$t 2>&1 | tee -a $t.log
     ./poke-cfg.sh $t
+    sed -i "s/$t/tN/g" $t.log
+    deactivate
 done
